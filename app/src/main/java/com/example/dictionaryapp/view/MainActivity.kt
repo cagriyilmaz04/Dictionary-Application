5d3289ee531971853d68a1e13f0726e5b005c767
cagriyilmaz04 <79268497+cagriyilmaz04@users.noreply.github.com>
2021-09-16 22:24:47 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/dictionaryapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dictionaryapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dictionaryapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
